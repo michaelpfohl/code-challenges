bc24a3ec7a3fc4d50856fd1d4bdc8a841a54a274
Michael Pfohl <rmpfohl@gmail.com>
2020-07-25 14:03:50 -0500

    change double quotations to single

--- Week3/Week3-JS-I.js
+++ Week3/Week3-JS-I.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
 //In these first 6 questions, replace `null` with the answer
 
 //1. create a string variable, it can contain anything
-const newString = "this is a string.";
+const newString = 'this is a string.';
 
 //2. create a number variable, it an be any number
 const newNum = 42;
